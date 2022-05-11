ed5efc594cb862d900da33c2f38e8b42b4f2fc48
jackyroad <you@example.com13762713527@qq.com>
2022-05-11 16:57:32 +0800

    com

--- 1.Python基础/2输入与输出.py
+++ 1.Python基础/2输入与输出.py
@@ -6,3 +6,10 @@ print("你输入的是：", a_name, "输入的类型是：", type(a_name), sep="
 print("你输入的是：{} ".format(a_name))
 # 元组类型数据输出
 print("你输入的是：{2},{1},{0}".format(*a_name2))
+
+# 对于赋值语句,赋值是·创造对象的引用而不是复制对象
+a_list = [1, 2, 3, 4]
+b_list = a_list
+a_list.reverse()  # a_list=[4,3,2,1]
+c_list = a_list
+print(a_list, b_list, c_list)  # 输出都为[4,3,2,1]
---
+++ 2.算法分析/4前、中、后序表达式.py
@@ -0,0 +1,3 @@
+a='-121'
+for i in range(len(a)):
+    print(a[::-(i+1)])
